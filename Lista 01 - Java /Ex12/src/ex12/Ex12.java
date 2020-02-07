ddaa4dacf2d255ace22a778b8d00fa761dfb4eb9
obraia <bryan22czo@gmail.com>
2020-02-06 22:06:57 -0300

    Aplicando de fato o POO Ex12

--- Lista 01 - Java /Ex12/src/ex12/Ex12.java
+++ Lista 01 - Java /Ex12/src/ex12/Ex12.java
@@ -5,16 +5,28 @@ import java.util.ArrayList;
 /**
  * @author bryan
  */
-
 class Aluno {
 
-    int Numero;
-    char[] Respostas;
-    int Nota;
+    private int Numero;
+    private char[] Respostas;
+    private int Nota;
 
     public Aluno(int numero, char[] respostas) {
-        Numero = numero;
-        Respostas = respostas;
+        this.Numero = numero;
+        this.Respostas = respostas;
+    }
+    
+    public int getNumero() { return this.Numero; }
+    public int getNota() { return this.Nota; }
+    
+    public void VerificarRespostas(char[] gabarito) {
+
+        for (int i = 0; i < this.Respostas.length; i++) {
+
+            if (gabarito[i] == this.Respostas[i]) {
+                this.Nota++;
+            }
+        }
     }
 }
 
@@ -27,7 +39,7 @@ public class Ex12 {
         int quantAprovados = 0;
         double porcentAprovados;
 
-        // --> Criar lista do tipo Aluno e adicionando alunos
+        // --> Criar lista do tipo Aluno e adicionar alunos
         ArrayList<Aluno> alunos = new ArrayList<>();
         alunos.add(new Aluno(05, new char[]{'B', 'C', 'E', 'B', 'A', 'D', 'C', 'A'}));
         alunos.add(new Aluno(10, new char[]{'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'}));
@@ -43,25 +55,20 @@ public class Ex12 {
         // --> Verificar respostas, aplicar notas aos alunos e fazer contagem de aprovados
         for (int i = 0; i < alunos.size(); i++) {
 
-            for (int j = 0; j < alunos.get(i).Respostas.length; j++) {
+            alunos.get(i).VerificarRespostas(gabarito);
 
-                if (gabarito[j] == alunos.get(i).Respostas[j]) {
-                    alunos.get(i).Nota++;
-                }
-            }
-
-            if (alunos.get(i).Nota >= 5) {
+            if (alunos.get(i).getNota() >= 5) {
                 quantAprovados++;
             }
         }
 
         // --> Informar notas e números dos alunos
         for (int i = 0; i < alunos.size(); i++) {
-            System.out.println(String.format("Aluno: %d - Nota: %d", alunos.get(i).Numero, alunos.get(i).Nota));
+            System.out.println(String.format("Aluno: %d - Nota: %d", alunos.get(i).getNumero(), alunos.get(i).getNota()));
         }
 
         porcentAprovados = (((double) quantAprovados / alunos.size()) * 100);
 
-        System.out.println(String.format("\nA porcentagem de alunos aprovados é %.2f %%", porcentAprovados));
+        System.out.println(String.format("\nA porcentagem de alunos aprovados é %.2f %% \n", porcentAprovados));
     }
 }
