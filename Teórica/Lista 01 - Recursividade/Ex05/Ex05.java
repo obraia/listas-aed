1d125936626b1bb7f352cc2f9cdf80a399afed75
obraia <bryan22czo@gmail.com>
2020-03-14 15:14:02 -0300

    Adiconado termo n Ex05

--- Teórica/Lista 01 - Recursividade/Ex05/Ex05.java
+++ Teórica/Lista 01 - Recursividade/Ex05/Ex05.java
@@ -1,17 +1,17 @@
 class Ex05 {
     public static void main(String[] args) {
-        System.out.println(calcularCosseno(0));
+        int n = 32; // --> Valor máximo
+        if (n % 2 != 0) n -= 1;
+        
+        System.out.println(calcularCosseno(n));
+    }
+    
+    public static double calcularCosseno(int n) {
+        if (n == 0) return 1;
+        else return (1.0 / fatorial(n*2)) - calcularCosseno(n - 1);
     }
 
-    public static double calcularCosseno( int denominador) {
-        if (denominador == 12) {
-            return (1.0 / fatorial(denominador));
-        } else {
-            return (1.0 / fatorial(denominador)) - calcularCosseno((denominador + 2));
-        }
-    }
-
-    public static int fatorial(int numero) {
+    public static long fatorial(int numero) {
         if (numero == 0) {
             return 1;
         } else {
